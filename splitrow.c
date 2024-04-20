0a6c1dd81c23f8b9999f7dee91d3cac7616bc976
Keith Muller <66692457+keithmuller@users.noreply.github.com>
2024-04-20 10:54:21 -0700

    Updated comments

--- splitrow.c
+++ splitrow.c
@@ -1,29 +1,34 @@
 /*
  * splitrow
  * 
- * usage:   input buffer in csv format, builds table of pointers to each field
- *          (1) walk the input row passed in buf. Set each element in the array
+ * usage:   input a row in csv format, builds table of pointers to each field
+ *          returns the number of columns found in the row.
+ *
+ * Operation:
+ *          (1) traverse the input row passed in buf. Set each element in the array
  *          of pointers (ptable) to point at the start of each column.
  *          (2) Replace the delimiter that follows each column to be a '\0' to
- *          terminate that row (making it a string)
- *          (3) Make sure you do not go past cnt entries in the ptarrays (where
- *          cnt is the expected number of columns in the row). When called with
- *          the header row, cnt will be the allocated size of the array ptable
- *          so the return value from this function (the number of columns found
- *          in the row buffer) will be used to determine the correct number of
- *          rows in every subsequent row in the input.
+ *          terminate that column (making it a string)
+ *          (3) Make sure you do not go past cnt entries in the ptable (where
+ *          cnt is the expected number of columns in the row).
+ *
+ *          When called with the header row, cnt is the number of elements of the
+ *          array ptable. The return value from this function (the number of columns
+ *          found in the row buffer) will be used to determine the correct number of
+ *          columns in every subsequent row in the input.
  *
  * argument description:
  *
  *  buf     csv format row stored in an array of characters, '\0' terminated
- *  ptable  points at array of pointers each element points at a data field
- *          in the row
- *  cnt     expected number cols in row. If buf contains more than this
- *          number of columns then return cnt + 1
- *  delim   the field delimiter             
+ *  ptable  points at array of pointers, each element points at the first character
+ *          of a column in the row
+ *  cnt     expected number of cols in row. If buf contains more than this
+ *          number of columns then return -1
+ *  delim   the field delimiter also \n delimits the last column and the row            
  *
  * return values:
- *         -1 if row is missing a newline (fgets truncated buf) (or other error)
+ *         -1 if row is missing a newline (fgets truncated buf) or the number
+ *         of columns in the row is greater than cnt
  *         otherwise return the number of columns found in the input row buf
  *
  * error messages:
